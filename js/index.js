05e325b995e639caff18ab3ed2f0eaca8ba5fceb
Felipe-Cavalari <fecavalaridev@gmail.com>
2025-03-17 22:57:23 -0300

    dev::Interface completa

--- js/index.js
+++ js/index.js
@@ -1,38 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const themeToggle = document.getElementById('theme-toggle');
-  const sunIcon = document.getElementById('sun-icon');
-  const moonIcon = document.getElementById('moon-icon');
-  
-  // Check for saved theme preference or use preferred color scheme
-  const savedTheme = localStorage.getItem('theme');
-  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  
-  // Set initial theme
-  if (savedTheme === 'dark' || (!savedTheme && prefersDark)) {
-    document.documentElement.classList.add('dark');
-    moonIcon.classList.add('hidden');
-    sunIcon.classList.remove('hidden');
-  } else {
-    document.documentElement.classList.remove('dark');
-    sunIcon.classList.add('hidden');
-    moonIcon.classList.remove('hidden');
-  }
-  
-  // Toggle theme when button is clicked
-  themeToggle.addEventListener('click', function() {
-    // Toggle dark class on html element
-    const isDark = document.documentElement.classList.toggle('dark');
-    
-    // Toggle icons
-    if (isDark) {
-      moonIcon.classList.add('hidden');
-      sunIcon.classList.remove('hidden');
+
+  const menuToggle = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const openMobileMenu = document.getElementById('open-mobile-menu');
+  const closeMobileMenu = document.getElementById('close-mobile-menu');
+
+  // Toggle menu when button is clicked
+  menuToggle.addEventListener('click', function() {
+    // Verifica se a classe 'hidden' está presente
+    if (mobileMenu.classList.contains('hidden')) {
+      // Se estiver, remove a classe 'hidden'
+      mobileMenu.classList.remove('hidden');
     } else {
-      sunIcon.classList.add('hidden');
-      moonIcon.classList.remove('hidden');
+      // Caso contrário, adiciona a classe 'hidden'
+      mobileMenu.classList.add('hidden');
     }
-    
-    // Save preference
-    localStorage.setItem('theme', isDark ? 'dark' : 'light');
   });
+
+
 });
