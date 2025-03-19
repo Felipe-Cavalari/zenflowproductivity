8025ecba7a447af5f67a2125e6d600e0fe1a0901
Felipe-Cavalari <fecavalaridev@gmail.com>
2025-03-18 22:54:01 -0300

    dev::Ajustes Finais V1

--- js/index.js
+++ js/index.js
@@ -4,18 +4,60 @@ document.addEventListener('DOMContentLoaded', function() {
   const mobileMenu = document.getElementById('mobile-menu');
   const openMobileMenu = document.getElementById('open-mobile-menu');
   const closeMobileMenu = document.getElementById('close-mobile-menu');
+  let isMobileMenuOpen = false;
 
-  // Toggle menu when button is clicked
+
+  function openMobileNav() {
+    mobileMenu.classList.remove('hidden', 'max-h-0');
+    mobileMenu.classList.add('max-h-full');
+    openMobileMenu.classList.add('hidden');
+    closeMobileMenu.classList.remove('hidden');
+  }
+
+  function closeMobileNav() {
+    mobileMenu.classList.remove('max-h-full');
+    mobileMenu.classList.add('hidden', 'max-h-0');
+    closeMobileMenu.classList.add('hidden');
+    openMobileMenu.classList.remove('hidden');
+  }
+
   menuToggle.addEventListener('click', function() {
-    // Verifica se a classe 'hidden' está presente
-    if (mobileMenu.classList.contains('hidden')) {
-      // Se estiver, remove a classe 'hidden'
-      mobileMenu.classList.remove('hidden');
-    } else {
-      // Caso contrário, adiciona a classe 'hidden'
-      mobileMenu.classList.add('hidden');
-    }
-  });
+    isMobileMenuOpen ? closeMobileNav() : openMobileNav();
+    isMobileMenuOpen = !isMobileMenuOpen;
+  })
+
+
+
+  
+
+  
+// Adiciona o scroll no btn "Começar Agora" da Section CTA
+const btnScrollCta = document.getElementById('btn-scroll-cta')
+btnScrollCta.addEventListener('click', () => {
+  const pricingElement = document.getElementById('pricing');
+    pricingElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+        
+    });
+})
+
+
+
+
+// Adiciona o alerta para caso alguem clique em compre agora antes do lançamento
+const btnAlertPricing = document.getElementById('btn-alert-pricing')
+btnAlertPricing.addEventListener('click', () => {
+    const alert = document.getElementById('alert');
+    alert.classList.remove('hidden')  // Exibe o alerta
+    setTimeout(closeAlert, 3000);  // Fecha o alerta após 3 segundos
+})
+
+// Função para fechar o alerta
+function closeAlert() {
+    const alert = document.getElementById('alert');
+    alert.classList.add('hidden')  // Esconde o alerta
+}
 
 
 });
