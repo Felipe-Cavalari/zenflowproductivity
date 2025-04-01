d22d1c9c151d59f48e280a210bc860a5226ee4c4
Felipe-Cavalari <fecavalaridev@gmail.com>
2025-04-01 20:02:43 -0300

    ajustes na Hero Section e novos recursos informados

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-  const menuToggle = document.getElementById('menu-toggle');
-  const mobileMenu = document.getElementById('mobile-menu');
-  const openMobileMenu = document.getElementById('open-mobile-menu');
-  const closeMobileMenu = document.getElementById('close-mobile-menu');
-  const mobileNavLink = document.querySelectorAll('#mobile-nav-link');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+  const openMobileMenu = document.getElementById("open-mobile-menu");
+  const closeMobileMenu = document.getElementById("close-mobile-menu");
+  const mobileNavLink = document.querySelectorAll("#mobile-nav-link");
   let isMobileMenuOpen = false;
-
-
-
+  const anoAtual = document.getElementById("ano-atual");
 
   function openMobileNav() {
     mobileMenu.classList.remove("hidden", "max-h-0");
@@ -17,75 +14,79 @@ document.addEventListener('DOMContentLoaded', function() {
     closeMobileMenu.classList.remove("hidden");
     menuToggle.setAttribute("aria-expanded", "true");
     isMobileMenuOpen = true;
-}
+  }
 
-function closeMobileNav() {
+  function closeMobileNav() {
     mobileMenu.classList.remove("max-h-full");
     mobileMenu.classList.add("hidden", "max-h-0");
     closeMobileMenu.classList.add("hidden");
     openMobileMenu.classList.remove("hidden");
     menuToggle.setAttribute("aria-expanded", "false");
     isMobileMenuOpen = false;
-}
+  }
 
-// Alternar menu ao clicar no botão
-menuToggle.addEventListener("click", function (event) {
+  // Alternar menu ao clicar no botão
+  menuToggle.addEventListener("click", function (event) {
     event.stopPropagation(); // Evita que o clique se propague para o document
     isMobileMenuOpen ? closeMobileNav() : openMobileNav();
-});
-
-// Fecha o menu ao clicar fora dele
-document.addEventListener("click", function (event) {
-    if (isMobileMenuOpen && !mobileMenu.contains(event.target) && !menuToggle.contains(event.target)) {
-        closeMobileNav();
-    }
-});
-
-// Fecha o menu ao clicar em qualquer link dentro dele
-mobileNavLink.forEach(link => {
-  link.addEventListener("click", function () {
-      closeMobileNav();
-  });});
-
-function scrollToPrice(){
-  const pricingElement = document.getElementById('pricing');
-  pricingElement.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
   });
-}
 
-  
+  // Fecha o menu ao clicar fora dele
+  document.addEventListener("click", function (event) {
+    if (
+      isMobileMenuOpen &&
+      !mobileMenu.contains(event.target) &&
+      !menuToggle.contains(event.target)
+    ) {
+      closeMobileNav();
+    }
+  });
 
-  
-// Adiciona o scroll no btn "Começar Agora" da Section CTA
-const btnScrollCta = document.getElementById('btn-scroll-cta')
-btnScrollCta.addEventListener('click', () => {
-  scrollToPrice()
-})
+  // Fecha o menu ao clicar em qualquer link dentro dele
+  mobileNavLink.forEach((link) => {
+    link.addEventListener("click", function () {
+      closeMobileNav();
+    });
+  });
 
-// adiciona o scroll no btc "Experimente agora"
-const btnScrollExperimentNow = document.getElementById('btn-experimente-agora')
-btnScrollExperimentNow.addEventListener('click', () => {
-  scrollToPrice()
-})
+  function scrollToPrice() {
+    const pricingElement = document.getElementById("pricing");
+    pricingElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
 
+  // Adiciona o scroll no btn "Começar Agora" da Section CTA
+  const btnScrollCta = document.getElementById("btn-scroll-cta");
+  btnScrollCta.addEventListener("click", () => {
+    scrollToPrice();
+  });
 
+  // adiciona o scroll no btc "Experimente agora"
+  const btnScrollExperimentNow = document.getElementById(
+    "btn-experimente-agora"
+  );
+  btnScrollExperimentNow.addEventListener("click", () => {
+    scrollToPrice();
+  });
 
+  // Adiciona o alerta para caso alguem clique em compre agora antes do lançamento
+  const btnAlertPricing = document.getElementById("btn-alert-pricing");
+  btnAlertPricing.addEventListener("click", () => {
+    const alert = document.getElementById("alert");
+    alert.classList.remove("hidden"); // Exibe o alerta
+    setTimeout(closeAlert, 3000); // Fecha o alerta após 3 segundos
+  });
 
-// Adiciona o alerta para caso alguem clique em compre agora antes do lançamento
-const btnAlertPricing = document.getElementById('btn-alert-pricing')
-btnAlertPricing.addEventListener('click', () => {
-    const alert = document.getElementById('alert');
-    alert.classList.remove('hidden')  // Exibe o alerta
-    setTimeout(closeAlert, 3000);  // Fecha o alerta após 3 segundos
-})
+  // Função para fechar o alerta
+  function closeAlert() {
+    const alert = document.getElementById("alert");
+    alert.classList.add("hidden"); // Esconde o alerta
+  }
 
-// Função para fechar o alerta
-function closeAlert() {
-    const alert = document.getElementById('alert');
-    alert.classList.add('hidden')  // Esconde o alerta
-}
-
-
+  // coleta o ano atual
+  const dataAtual = new Date();
+  const ano = dataAtual.getFullYear();
+  anoAtual.innerText = ano;
 });
